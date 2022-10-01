c3d82fdf1046d8d15274f70e5608c96c6cab919f
Jeff Thomas <jeffrey.craig.thomas@gmail.com>
2022-09-30 20:39:18 -0800

    cors

--- helpers.py
+++ helpers.py
@@ -10,16 +10,13 @@ def token_required(our_flask_function):
     def decorated(*args, **kwargs):
         token = None
 
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token'].split(' ')[1]
-
-        if not 'x-access-token' in request.headers:
+        if not 'xaccesstoken' in request.headers:
             return jsonify({'message':'x-access-token header is missing'}), 401
         else:
-            token = request.headers['x-access-token'].split(' ')[1]
+            token = request.headers['xaccesstoken'].split(' ')[1]
 
         if not token:
-            return jsonify({'message':'invalid token header: '+request.headers['x-access-token']}), 401
+            return jsonify({'message':'invalid token header: '+request.headers['xaccesstoken']}), 401
 
         try:
             current_user_token = User.query.filter_by(token=token).first()
