63800d939cd1c36653e938d065a22b455c4a8641
Jeff Thomas <jeffrey.craig.thomas@gmail.com>
2022-09-30 20:42:52 -0800

    cors

--- helpers.py
+++ helpers.py
@@ -11,7 +11,7 @@ def token_required(our_flask_function):
         token = None
 
         if not 'xaccesstoken' in request.headers:
-            return jsonify({'message':'x-access-token header is missing'}), 401
+            return jsonify(request.headers), 401
         else:
             token = request.headers['xaccesstoken'].split(' ')[1]
 
